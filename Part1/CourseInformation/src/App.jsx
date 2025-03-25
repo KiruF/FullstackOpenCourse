4efdce0d8406d071445ef427cfcac3412b79a98b
KiruF <kfurmanchuk@gmail.com>
2025-03-25 12:40:16 +0100

    Exercise 1.5 Completed

--- Part1/CourseInformation/src/App.jsx
+++ Part1/CourseInformation/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) =>
-  <h1>{props.course}</h1>
+  <h1>{props.course.name}</h1>
 
 const Content = (props) => {
   return (
@@ -11,7 +11,7 @@ const Content = (props) => {
   )
 }
 
-const Total = (props) => {
+const Total = (props) => {  
   const partArray = props.parts
   const sum =
     partArray[0].exercises +
@@ -27,28 +27,30 @@ const Part = (props) =>
   <p>{props.part.name} {props.part.exercises}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
-    <div>
+    <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
   )
 }
 
