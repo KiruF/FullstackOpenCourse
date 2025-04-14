d665b4b22b8021fdf7102c4b742dda7301de795b
KiruF <kfurmanchuk@gmail.com>
2025-04-14 17:06:36 +0200

    Exercise 1.14 Completed

--- Part1/anecdotes/src/App.jsx
+++ Part1/anecdotes/src/App.jsx
@@ -3,6 +3,27 @@ import { useState } from 'react'
 const GetRandomInt = (maxValue) =>
   Math.floor(Math.random() * maxValue)
 
+const TopAnecdote = ({ title, votesCount, topIndex, anecdotes }) => {
+  if (topIndex === -1)
+    return
+
+  return (
+    <Anecdote
+      title={title}
+      anecdote={anecdotes[topIndex]}
+      votesCount={votesCount} />
+  )
+}
+
+const Anecdote = ({ title, anecdote, votesCount }) =>
+  <div>
+    <h2>{title}</h2>
+    <p>
+      {anecdote}<br />
+      {`has ${votesCount} votes`}
+    </p>
+  </div>
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -19,31 +40,58 @@ const App = () => {
 
   const [selected, setSelected] = useState(GetRandomInt(maxIndex))
   const [votes, setVotes] = useState(Array(maxIndex).fill(0))
+  const [topIndex, setTop] = useState(-1)
 
   const handleNextAnecdote = () => {
     // Initialise random index
     let randomIndex = GetRandomInt(maxIndex)
 
     // Ensure that the generated index is not the same as the previous one
-    while (randomIndex === selected) {
+    // Safeguard to avoid infinite loop
+    let counter = 0;
+    while (randomIndex === selected && counter < 100) {
       randomIndex = GetRandomInt(maxIndex)
+      counter++
     }
     // Update selected and rerender the page
     setSelected(randomIndex)
   }
 
-  const handleVote = () =>{
+  const handleVote = () => {
     const votesCopy = [...votes]
     votesCopy[selected]++
 
     setVotes(votesCopy)
+
+    updateTop(votesCopy)
+  }
+
+  const updateTop = (votes) => {
+
+    let currentTopIndex = topIndex
+    let maxVotes = currentTopIndex === -1
+      ? 0
+      : votes[topIndex]
+
+    for (let index = 0; index < votes.length; index++) {
+      const votesCount = votes[index]
+      if (maxVotes < votesCount) {
+        maxVotes = votesCount
+        currentTopIndex = index
+      }
+    }
+
+    if (currentTopIndex != topIndex) {
+      setTop(currentTopIndex)
+    }
   }
 
   return (
     <div>
-
-      <p>{anecdotes[selected]}</p>
-      <p>{`has ${votes[selected]} votes`}</p>
+      <Anecdote
+        title='Anecdote of the day'
+        anecdote={anecdotes[selected]}
+        votesCount={votes[selected]} />
 
       <button onClick={handleVote}>
         vote
@@ -52,6 +100,12 @@ const App = () => {
       <button onClick={handleNextAnecdote}>
         next anecdote
       </button>
+
+      <TopAnecdote
+        topIndex={topIndex}
+        anecdotes={anecdotes}
+        title='Anecdote with most votes'
+        votesCount={votes[topIndex]} />
     </div>
   )
 }
