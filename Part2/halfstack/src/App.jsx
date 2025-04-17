a2c130ca867b5c051653d53be6ae3c26b72716ec
KiruF <kfurmanchuk@gmail.com>
2025-04-17 15:17:51 +0200

    Turns out useId should not be used to generate keys for a list, I'm bringing back the approach from the exercise

--- Part2/halfstack/src/App.jsx
+++ Part2/halfstack/src/App.jsx
@@ -1,47 +1,46 @@
 import Course from './components/Course'
-import {useId} from 'react'
 
 const App = () => {
   const courses = [
     {
       name: 'Half Stack application development',
-      id: useId(),
+      id: 1,
       parts: [
         {
           name: 'Fundamentals of React',
           exercises: 10,
-          id: useId()
+          id: 1
         },
         {
           name: 'Using props to pass data',
           exercises: 7,
-          id: useId()
+          id: 2
         },
         {
           name: 'State of a component',
           exercises: 14,
-          id: useId()
+          id: 3
         },
         {
           name: 'Redux',
           exercises: 11,
-          id: useId()
+          id: 4
         }
       ]
-    },
+    }, 
     {
       name: 'Node.js',
-      id: useId(),
+      id: 2,
       parts: [
         {
           name: 'Routing',
           exercises: 3,
-          id: useId()
+          id: 1
         },
         {
           name: 'Middlewares',
           exercises: 7,
-          id: useId()
+          id: 2
         }
       ]
     }
