fb1f479f1cb2b40f49ed92bb14cc3f5eb40c7411
KiruF <kfurmanchuk@gmail.com>
2025-04-18 17:17:33 +0200

    Exercise 2.6 Completed

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,22 +3,45 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    if (newName.length === 0){
+      alert('Enter a name, please, before adding a new Person to the phonebook')
+      return
+    }
+
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+
+  const handleNewNameInput = (event) =>
+    setNewName(event.target.value)
 
   return (
     <div>
+
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleNewNameInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <div key={person.name}>
+          {person.name}
+        </div>)}
+
     </div>
   )
 }
